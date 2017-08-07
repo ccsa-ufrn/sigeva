641ac66b96e22cb1564faae7a6d565bd727239f3
Maradona Morais <maradona.morais@hotmail.com>
2017-08-07 15:12:15 -0300

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,36 @@
 var path = require("path");
 
-const config = {
+const common = {
 	devtool: "eval-source-map",
-	entry: path.join(__dirname, "/lib/main.js"),
-	output: {
-		path: path.join(__dirname, "/dist/"),
-		filename: "bundle.js",
-		publicPath: "/"
-	},
 	module: {
 		loaders: [{
 			test: /\.js?$/,
 			exclude: /node_modules/,
 			loader: "babel-loader"
 		}]
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
 	}
 };
 
-module.exports = config;
+const front = {
+	entry: path.join(__dirname, "/lib/front/main.js"),
+	output: {
+		path: path.join(__dirname, "/dist/front"),
+		filename: "output.js"
+	}
+};
+
+const back = {
+	entry: path.join(__dirname, "/lib/back/main.js"),
+	output: {
+		path: path.join(__dirname, "/dist/back"),
+		filename: "output.js"
+	}
+}
+
+module.exports = [
+	Object.assign({}, common, front),
+	Object.assign({}, common, back)
+];
