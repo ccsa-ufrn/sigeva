6f40ad73de4ff5cb207c9bfc6b543a379d443c21
Maradona Morais <me@mrmorais.com.br>
2017-08-13 17:29:28 -0300

    React.js setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,12 @@
 var path = require("path");
 
-const common = {
+const react = {
 	devtool: "eval-source-map",
+	entry: path.join(__dirname, "/src/components/app.js"),
+	output: {
+		path: path.join(__dirname, "/public/assets/js"),
+		filename: "bundle.js"
+	},
 	module: {
 		loaders: [{
 			test: /\.js?$/,
@@ -12,41 +17,6 @@ const common = {
 	resolve: {
 		extensions: ['.js', '.jsx']
 	},
-	node: {
-		fs: 'empty',
-		module: 'empty',
-		net: 'empty',
-		tls: 'empty'
-	}
 };
 
-const front = {
-	entry: path.join(__dirname, "/src/front/main.js"),
-	output: {
-		path: path.join(__dirname, "/dist/front"),
-		filename: "output.js"
-	}
-};
-
-const back = {
-	entry: path.join(__dirname, "/src/controllers/main.js"),
-	output: {
-		path: path.join(__dirname, "/dist/back"),
-		filename: "output.js"
-	}
-};
-
-// TODO: Each service must exports one separated file
-const services = {
-	entry: path.join(__dirname, "/src/services/main.js"),
-	output: {
-		path: path.join(__dirname, "/dist/service"),
-		filename: "service.js"
-	}
-};
-
-module.exports = [
-	Object.assign({}, common, front),
-	Object.assign({}, common, back),
-	Object.assign({}, common, services)
-];
+module.exports = react;
