820ec694e64a70bf1f3645a8f87bad1b31729319
Maradona Morais <me@mrmorais.com.br>
2017-09-16 23:46:24 -0300

    write new tests :watch:

--- src/tests/event.js
+++ src/tests/event.js
@@ -10,54 +10,59 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('Event', () => {
+  let server;
+  beforeEach(() => {
+    server = app.listen(4000);
+  });
+
+  afterEach(() => {
+    server.close();
+  });
   describe('POST /api/event', () => {
-    let server;
-    beforeEach(() => {
-      server = app.listen(4000);
-    });
-
-    afterEach(() => {
-      server.close();
-    });
-
     it('Quando tentar acessar a rota por GET deve retornar erro', (done) => {
       chai.request(server)
         .get('/api/event/')
         .end((err, res) => {
           expect(res).to.have.status(404);
+          expect(res.body.error).to.be.true;
           done();
         });
     });
 
     it('Quando tentar criar um evento sem nenhum campo deve retornar erro', (done) => {
       chai.request(server)
-        .post('/event/')
+        .post('/api/event')
         .end((err, res) => {
           expect(res).to.have.status(400);
           expect(res.body).to.not.be.undefined;
           expect(res.body.error).to.be.true;
           expect(res.body.error_info).to.not.be.undefined;
-          expect(res.body.data).to.be.empty;
+          expect(res.body.data).to.not.be.empty;
+          expect(res.body.data[0]).to.have.property('field');
+          expect(res.body.data[0]).to.have.property('message');
           done();
         });
     });
 
     it('Quando tentar criar um evento incompleto deve retornar erro', (done) => {
       chai.request(server)
-        .post('/event/')
+        .post('/api/event/')
         .set('Content-Type', 'application/json')
         .send({ name: 'incompleto' })
         .end((err, res) => {
           expect(res).to.have.status(400);
           expect(res.body.error).to.be.true;
-          expect(res.body.data).to.be.empty;
+          expect(res.body.error_info).to.not.be.undefined;
+          expect(res.body.data).to.not.be.empty;
+          expect(res.body.data[0]).to.have.property('field');
+          expect(res.body.data[0]).to.have.property('message');
           done();
         });
     });
 
     it ('Quando passar valores vazios deve retornar erro', (done) => {
       chai.request(server)
-        .post('/event/')
+        .post('/api/event/')
         .set('Content-Type', 'application/json')
         .send({ name: '', subtitle: '' })
         .end((err, res) => {
--- src/tests/user.js
+++ src/tests/user.js
@@ -1,8 +1,10 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
+import bcrypt from 'bcrypt';
+import mongoose from 'mongoose';
 
 import app from '../controllers/App';
-import eventModel from '../models/event.model';
+import userModel from '../models/user.model';
 
 const should = chai.should;
 const expect = chai.expect;
@@ -10,16 +12,21 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('User', () => {
+  let server;
+  beforeEach(() => {
+    userModel.remove({}, () => {
+      const senha = bcrypt.hashSync('senha', 10);
+      const user  = new userModel({ name: 'Teste', email: 'teste@hot.com', password:senha});
+      user.save(() => { });
+    }); // Remove all users from the collection
+    server = app.listen(4000);
+  });
+
+  afterEach(() => {
+    server.close();
+  });
+
   describe('POST /api/user', () => {
-    let server;
-    beforeEach(() => {
-      server = app.listen(4000);
-    });
-
-    afterEach(() => {
-      server.close();
-    });
-
     it('Deve criar um usuário simples', (done) => {
       const usuario = {
         name: 'Maradona',
@@ -33,42 +40,88 @@ describe('User', () => {
         .end((err, res) => {
           expect(res).to.have.status(200);
           expect(res.body.error).to.be.false;
+          expect(res.body.data).to.not.be.empty;
+          expect(res.body.data.name).to.be.equal(usuario.name);
+          expect(res.body.data.email).to.be.equal(usuario.email);
+          done();
         })
     });
-    it('should not create with missing fields');
-    it('should not accept extraneous fileds');
-    it('should not create a user with incorrect types in fields');
-    it('should not create a user if alredy exists a user with email');
+
+    it('Não deve criar usuário com campos faltando', (done) => {
+      chai.request(server)
+        .post('/api/user')
+        .set('Content-Type', 'application/json')
+        .send({ name: 'incompleto' })
+        .end((err, res) => {
+          expect(res).to.have.status(400);
+          expect(res.body.error).to.be.true;
+          expect(res.body.data).to.not.be.empty;
+          expect(res.body.data[0]).to.have.property('field');
+          expect(res.body.data[0]).to.have.property('message');
+          done();
+        })
+    });
+
+    it('Não deve criar usuário com email existente', (done) => {
+      chai.request(server)
+      .post('/api/user')
+      .set('Content-Type', 'application/json')
+      .send({ name: 'Maradona', password:'morais', email: 'teste@hot.com' })
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body.error).to.be.true;
+        expect(res.body.data).to.not.be.empty;
+        expect(res.body.data[0]).to.have.property('field');
+        expect(res.body.data[0]).to.have.property('message');
+        done();
+      });
+
+    });
   });
 
-  describe('Deactivating a User', () => {
-    it('should desable a user');
-    it('should not disable a user if he is the last administrator');
-    it('should not disable a user if he is the last event coordinator');
+  describe('POST /api/user/authorize',() => {
+    describe('Authenticating Users', () => {
+      it('Deve autenticar um usuário', () => {
+        chai.request(server)
+        .post('/api/user/authorize')
+        .set('Content-Type', 'application/json')
+        .send({ email: 'teste@hot.com', password:'senha' })
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.body.error).to.be.false;
+          expect(res.body.data).to.not.be.empty;
+          expect(res.body.data).to.have.property('token');
+          done();
+        });
+      });
+      it('should not authenticate unactivated user');
+      it('should not authenticate wrongs credentials/inexisting users');
+    });
   });
-
-  describe('Editing a User', () => {
-    it('should edit user fields');
-    it('should not edit password without actual password verification');
-    it('should not edit extraneous fields from user');
-    it('should not be edited by other user unless the own or administrator');
-  });
-
-  describe('Getting a User by ID', () => {
-    it('should return a user');
-    it('should return a error if user doesn\'t exists');
-  });
-
-  describe('Getting all Users', () => {
-    it('should return a set of users');
-    it('should return only enrolled users if the requester is a event coordinator');
-    it('should return all users if the requester is a administrator');
-  });
-
-  describe('Authenticating Users', () => {
-    it('should authenticate a user');
-    it('should not authenticate unactivated user');
-    it('should not authenticate wrongs credentials/inexisting users');
-  });
-
 });
+
+
+
+  // describe('Deactivating a User', () => {
+  //   it('should desable a user');
+  //   it('should not disable a user if he is the last administrator');
+  //   it('should not disable a user if he is the last event coordinator');
+  // });
+
+  // describe('Editing a User', () => {
+  //   it('should edit user fields');
+  //   it('should not edit password without actual password verification');
+  //   it('should not edit extraneous fields from user');
+  //   it('should not be edited by other user unless the own or administrator');
+  // });
+
+  // describe('Getting a User by ID', () => {
+  //   it('should return a user');
+  //   it('should return a error if user doesn\'t exists');
+  // });
+
+  // describe('Getting all Users', () => {
+  //   it('should return a set of users');
+  //   it('should return only enrolled users if the requester is a event coordinator');
+  //   it('should return all users if the requester is a administrator');
+  // });
