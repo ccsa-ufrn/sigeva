80140a12fe43a8dc20e4d620fca603ef4df451cd
Maradona Morais <me@mrmorais.com.br>
2017-09-29 13:05:33 -0300

    new 404 error page

--- src/components/error/Error404.jsx
+++ src/components/error/Error404.jsx
@@ -4,21 +4,17 @@ import MainLayout from '../layout/MainLayout';
 class Error404 extends Component {
   render() {
     const page = (
-      <div><h2>Error 404</h2></div>
+      <div id='error404' className='container-fluid'>
+        <div className='row'>
+          <div className='col-md-12'>
+            <div id='error-text'>
+              <h3>404. Página não encontrada</h3>
+              <p>Desculpa, mas a página que você está procurando não foi encontrada. Verifique a URL e tente novamente.</p>
+            </div>
+          </div>
+        </div>
+      </div>
     );
-
-    const error404Path = [
-      {
-        active: false,
-        name: 'Início',
-        address: '/',
-      },
-      {
-        active: true,
-        name: 'Registro',
-        address: '/register',
-      },
-    ];
 
     return (<MainLayout children={page} />);
   }
