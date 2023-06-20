bfe8fb75727b58c92b3db0c14adac90ca48371b5
Luccas Mateus de Medeiros Gomes <luccasmmg@gmail.com>
2023-06-20 11:14:15 -0300

    Update nodemailer

--- src/controllers/user/UserRouter.js
+++ src/controllers/user/UserRouter.js
@@ -1,12 +1,12 @@
-import { Router } from 'express';
-import JWT from 'jsonwebtoken';
-import NodeMailer from 'nodemailer';
+import { Router } from "express";
+import JWT from "jsonwebtoken";
+import NodeMailer from "nodemailer";
 
-import Response from '../Response';
-import { simpleAuthorization } from '../authorization/Authorization';
-import { secret, email } from '../../../config';
+import Response from "../Response";
+import { simpleAuthorization } from "../authorization/Authorization";
+import { secret, email } from "../../../config";
 
-import User from './User';
+import User from "./User";
 
 /**
  * User Router
@@ -20,21 +20,27 @@ const userRouter = Router();
  * @param fields representing a user
  * @return Created user object
  */
-userRouter.post('/', (req, res) => {
+userRouter.post("/", (req, res) => {
   const user = new User();
-  user.setData(req.body)
+  user
+    .setData(req.body)
     .then(() => {
-      user.store()
+      user
+        .store()
         .then((data) => {
           res.json(Response(false, data));
-        }).catch((err) => {
+        })
+        .catch((err) => {
           res.json(Response(true, {}, err));
         });
-    }).catch((data) => {
-      if (data.error_type === 'system') {
+    })
+    .catch((data) => {
+      if (data.error_type === "system") {
         res.status(500).json(Response(true, {}, data.message));
       } else {
-        res.status(400).json(Response(true, data.errors, 'Erro ao fazer cadastro'));
+        res
+          .status(400)
+          .json(Response(true, data.errors, "Erro ao fazer cadastro"));
       }
     });
 });
@@ -43,16 +49,14 @@ userRouter.post('/', (req, res) => {
  * Update user data
  * @param
  */
-userRouter.put('/', simpleAuthorization, (req, res) => {
+userRouter.put("/", simpleAuthorization, (req, res) => {
   const user = new User();
 
-  user.loadById(res.locals.user._id)
-    .then(() => {
-      user.updateData(req.body)
-        .then(() => {
-          res.json(Response(false, {}));
-        });
+  user.loadById(res.locals.user._id).then(() => {
+    user.updateData(req.body).then(() => {
+      res.json(Response(false, {}));
     });
+  });
 });
 
 /**
@@ -60,12 +64,13 @@ userRouter.put('/', simpleAuthorization, (req, res) => {
  * @param email user email passed by post body
  * @param password user password passed by post body
  */
-userRouter.post('/authorize', (req, res) => {
+userRouter.post("/authorize", (req, res) => {
   const email = req.body.email; // user passed email
   const password = req.body.password; // user passed password
 
   const user = new User();
-  user.authorize(email, password)
+  user
+    .authorize(email, password)
     .then((authorized) => {
       if (authorized) {
         // User was authorized
@@ -75,17 +80,19 @@ userRouter.post('/authorize', (req, res) => {
         };
         // Creates a JWT to grant user access
         const token = JWT.sign(tokenData, secret, {
-          expiresIn: '24h', // The token expires in 24 hours
+          expiresIn: "24h", // The token expires in 24 hours
         });
-        res.cookie('sigeva_user_token', token).json(Response(false, { authorized, token }));
+        res
+          .cookie("sigeva_user_token", token)
+          .json(Response(false, { authorized, token }));
       } else {
         // Passed password is incorrect
-        res.json(Response(true, {}, 'Senha incorreta'));
+        res.json(Response(true, {}, "Senha incorreta"));
       }
     })
     .catch((err) => {
       // The user with passed email doesn't exists
-      res.json(Response(true, {}, 'Não existe usuário com este email'));
+      res.json(Response(true, {}, "Não existe usuário com este email"));
     });
 });
 
@@ -93,18 +100,23 @@ userRouter.post('/authorize', (req, res) => {
  * Return the formated current logged user
  * @return User Object
  */
-userRouter.get('/me', simpleAuthorization, (req, res) => {
+userRouter.get("/me", simpleAuthorization, (req, res) => {
   const user = new User();
-  user.loadById(res.locals.user._id) // Load from id validated by simpleAuthorization
+  user
+    .loadById(res.locals.user._id) // Load from id validated by simpleAuthorization
     .then(() => {
       // The user exists
-      user.toFormatedUser('cpf institution phone special-needs')
+      user
+        .toFormatedUser("cpf institution phone special-needs")
         .then((formatedUser) => {
           res.json(Response(false, formatedUser));
-        }).catch((e) => {
-          res.json(Response(true, {
-            err: e.toString(),
-          }));
+        })
+        .catch((e) => {
+          res.json(
+            Response(true, {
+              err: e.toString(),
+            })
+          );
         });
     });
 });
@@ -112,28 +124,34 @@ userRouter.get('/me', simpleAuthorization, (req, res) => {
 /**
  * Close the user session of the current logged user
  */
-userRouter.get('/logout', simpleAuthorization, (req, res) => {
+userRouter.get("/logout", simpleAuthorization, (req, res) => {
   try {
-    res.clearCookie('sigeva_user_token').json(Response(false, {}));
+    res.clearCookie("sigeva_user_token").json(Response(false, {}));
   } catch (e) {
-    res.json(Response(true, {}, 'Erro desconhecido ao fechar sessão de usuário'));
+    res.json(
+      Response(true, {}, "Erro desconhecido ao fechar sessão de usuário")
+    );
   }
 });
 
 /**
  * Generate a code to recover password
  */
-userRouter.post('/recover-password', (req, res) => {
+userRouter.post("/recover-password", (req, res) => {
   const user = new User();
   const userEmail = req.body.email;
 
-  user.loadByEmail(userEmail)
+  user
+    .loadByEmail(userEmail)
     .then(() => {
-      user.generateRecoverPasswordCode()
+      user
+        .generateRecoverPasswordCode()
         .then((generatedCode) => {
           // send mail here
           const transporter = NodeMailer.createTransport({
-            service: 'gmail',
+            host: "https://mx5.ufrn.br",
+            port: 465,
+            secure: true, // upgrade later with STARTTLS
             auth: {
               user: email.user,
               pass: email.password,
@@ -142,7 +160,7 @@ userRouter.post('/recover-password', (req, res) => {
           const mailOptions = {
             from: email.user,
             to: userEmail,
-            subject: '[Sigeva] Recuperação de Senha',
+            subject: "[Sigeva] Recuperação de Senha",
             html: `<h4>Solicitação de nova senha</h4>
             <p>Foi solicitada a criação de uma nova senha para a conta com este e-mail na plataforma
             Sigeva
@@ -156,27 +174,31 @@ userRouter.post('/recover-password', (req, res) => {
             res.json(Response(false, {}));
           });
         })
-        .catch(() => { res.json(Response(true, {})); });
+        .catch(() => {
+          res.json(Response(true, {}));
+        });
     })
     .catch(() => {
-      res.json(Response(true, {}, 'Usuário não encontrado'));
+      res.json(Response(true, {}, "Usuário não encontrado"));
     });
 });
 
 /**
  * Set new password
  */
-userRouter.post('/new-password', (req, res) => {
+userRouter.post("/new-password", (req, res) => {
   const user = new User();
   const password = req.body.newPassword;
   const userId = req.body.userId;
   const code = req.body.code;
 
-  user.loadById(userId)
+  user
+    .loadById(userId)
     .then(() => {
       if (user.userObject.passwordCode) {
         if (user.userObject.passwordCode === code) {
-          user.newPassword(password)
+          user
+            .newPassword(password)
             .then(() => {
               res.json(Response(false, {}));
             })
@@ -187,7 +209,7 @@ userRouter.post('/new-password', (req, res) => {
       }
     })
     .catch(() => {
-      res.json(Response(true, {}, 'Usuário não encontrado'));
+      res.json(Response(true, {}, "Usuário não encontrado"));
     });
 });
 
@@ -196,25 +218,27 @@ userRouter.post('/new-password', (req, res) => {
  * @param field user fields to be returned in request
  * @return User Object
  */
-userRouter.get('/:id', (req, res) => {
+userRouter.get("/:id", (req, res) => {
   const id = req.params.id;
-  const fields = (req.query.f) ? req.query.f : '_id'; /* return id by default */
+  const fields = req.query.f ? req.query.f : "_id"; /* return id by default */
 
   const user = new User(); // Create a user instance
-  user.loadById(id) // Load the user by the passed ID
+  user
+    .loadById(id) // Load the user by the passed ID
     .then(() => {
       // there is a user with that ID
-      user.toFormatedUser(fields) // Format it
+      user
+        .toFormatedUser(fields) // Format it
         .then((formatedUser) => {
           res.json(Response(false, formatedUser));
         })
         .catch(() => {
-          res.json(Response(true, {}, 'Erro ao formatar usuário'));
+          res.json(Response(true, {}, "Erro ao formatar usuário"));
         });
     })
     .catch(() => {
       // there is not a user with that ID, display error message
-      res.json(Response(true, {}, 'Usuário não encontrado'));
+      res.json(Response(true, {}, "Usuário não encontrado"));
     });
 });
 
